863422cc96d4e848ce5b4d3ac9e2a7e4339c23b2
Chayapol Moemeng <mchayapol@gmail.com>
2023-01-14 22:27:34 +0700

    Restructured

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,9 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
 var app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Sample Blog using Express.js App listening on port ${port}!`));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
